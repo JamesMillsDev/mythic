55d014f4633704d81e6707ecc0aa66e348141b0e
James Mills <jcmills.developer@gmail.com>
2026-04-20 16:32:06 +1000

    Initial commit

---
+++ Source/MythicDevEnv/MythicDevEnv.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MythicDevEnv.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MythicDevEnv, "MythicDevEnv" );
